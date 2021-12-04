2446e4891328ff13cbbbadee639abab52d5a02b7
Wieland Morgenstern <wieland.morgenstern@hhi.fraunhofer.de>
2021-12-04 19:35:20 +0100

    day4: combine day4a and day4b in generator

--- day4/day4.py
+++ day4/day4.py
@@ -18,61 +18,50 @@ def read_bingo(file):
     return drawn, boards
 
 
-@print_durations
-def day4a(file):
-
-    drawn, boards = read_bingo(file)
+def eval_bingo(draws, boards):
 
     matches = np.zeros_like(boards, dtype=bool)
-
-    for draw in drawn:
-        matches[boards == draw] = True
-
-        has_match_a1 = matches.all(axis=1).any(axis=1)
-        has_match_a2 = matches.all(axis=2).any(axis=1)
-
-        has_match = has_match_a1 + has_match_a2
-
-        if has_match.any():
-            winning_board_id = has_match.argmax()
-            winning_board = boards[winning_board_id]
-            winning_matches = matches[winning_board_id]
-            return winning_board[~winning_matches].sum() * draw
-
-
-@print_durations
-def day4b(file):
-
-    drawn, boards = read_bingo(file)
-
-    matches = np.zeros_like(boards, dtype=bool)
-
     has_won = np.zeros(boards.shape[0], dtype=bool)
 
-    for draw in drawn:
+    for draw in draws:
         matches[boards == draw] = True
 
         has_match_a1 = matches.all(axis=1).any(axis=1)
         has_match_a2 = matches.all(axis=2).any(axis=1)
 
-        has_match = has_match_a1 + has_match_a2
+        has_match = has_match_a1 | has_match_a2
 
-        if has_match.sum() == boards.shape[0] and has_won.sum() < boards.shape[0]:
-            winning_board_id = has_won.argmin()
+        new_winner = has_match & ~has_won
+
+        if new_winner.any():
+            winning_board_id = new_winner.argmax()
             winning_board = boards[winning_board_id]
             winning_matches = matches[winning_board_id]
-            return winning_board[~winning_matches].sum() * draw
+            yield winning_board[~winning_matches].sum() * draw
 
-        has_won += has_match
+        has_won = has_match
+
+
+@print_durations
+def day4(file):
+    draws, boards = read_bingo(file)
+    bingo_winner_generator = eval_bingo(draws, boards)
+    first = next(bingo_winner_generator)
+    for last in bingo_winner_generator:
+        pass
+    return first, last
 
 
 if __name__ == "__main__":
 
-    print(f"Day 4a: {day4a('input.txt')}")
-    print(f"Day 4b: {day4b('input.txt')}")
+    day4a_test, day4b_test = day4("test_input.txt")
+    day4a_puzzle, day4b_puzzle = day4("input.txt")
 
-    assert day4a("test_input.txt") == 4512
-    assert day4b("test_input.txt") == 1924
+    print(f"Day 4a: {day4a_puzzle}")
+    print(f"Day 4b: {day4b_puzzle}")
 
-    assert day4a("input.txt") == 89001
-    assert day4b("input.txt") == 7296
+    assert day4a_test == 4512
+    assert day4b_test == 1924
+
+    assert day4a_puzzle == 89001
+    assert day4b_puzzle == 7296
