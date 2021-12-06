e6e0ad1fb5cbe32fa494aa6076080632867579da
Wieland Morgenstern <wieland.morgenstern@hhi.fraunhofer.de>
2021-12-06 19:59:45 +0100

    day6: clean up code

--- day6/day6.py
+++ day6/day6.py
@@ -1,37 +1,48 @@
-from tqdm import trange
+from funcy import print_durations
+from collections import Counter, defaultdict
+import numpy as np
 
-with open(file, "r") as f:
-    line = f.readline()
 
-init = [int(x) for x in line.strip().split(",")]
+@print_durations
+def day6(file, num_simulation_days):
 
-init = [0]
+    # you can model each fish as a single number that represents
+    # the number of days until it creates a new lanternfish
+    fish_days = np.loadtxt(file, delimiter=",", dtype=int)
 
-for day in trange(256):
-    new = []
-    for fish in init:
-        if fish == 0:
-            new.append(6)
-            new.append(8)
-        else:
-            new.append(fish - 1)
-    init = new
+    # count fish by their countdown day
+    fish_day_counter = dict(Counter(fish_days))
 
-    print(len(init))
+    for day in range(num_simulation_days):
 
-dcc = dict(Counter(init))
+        updated_counter = defaultdict(int)
 
-for day in trange(256):
+        for day_countdown, numfish in fish_day_counter.items():
+            if day_countdown == 0:
+                # a lanternfish that creates a new fish resets its timer to 6
+                updated_counter[6] += numfish
+                # the new lanternfish starts with an internal timer of 8
+                updated_counter[8] += numfish
+            else:
+                updated_counter[day_countdown - 1] += numfish
 
-    nd = defaultdict(int)
+        fish_day_counter = updated_counter
 
-    for day, numfish in dcc.items():
-        if day == 0:
-            nd[6] += numfish
-            nd[8] += numfish
-        else:
-            nd[day - 1] += numfish
+    return sum(fish_day_counter.values())
 
-    dcc = nd
 
-print(sum(dcc.values()))
+if __name__ == "__main__":
+    test_a, test_b = day6("test_input.txt", 80), day6("test_input.txt", 256)
+    puzzle_a, puzzle_b = day6("input.txt", 80), day6("input.txt", 256)
+
+    print(f"Day 6a: {test_a}")
+    print(f"Day 6b: {test_b}")
+
+    print(f"Day 6a: {puzzle_a}")
+    print(f"Day 6b: {puzzle_b}")
+
+    assert test_a == 5934
+    assert test_b == 26984457539
+
+    assert puzzle_a == 365862
+    assert puzzle_b == 1653250886439
