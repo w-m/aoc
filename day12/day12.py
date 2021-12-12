51bd2cfd4e49400cf7d331bb2dc751c89b211823
Wieland Morgenstern <wieland.morgenstern@hhi.fraunhofer.de>
2021-12-12 14:57:48 +0100

    day12: dataclass to keep track of path and path state - runtime 5s -> 2.8s

--- day12/day12.py
+++ day12/day12.py
@@ -1,6 +1,18 @@
 from funcy import print_durations
 import networkx as nx
-from collections import Counter
+from dataclasses import dataclass
+
+
+@dataclass
+class Path:
+    """Keep a path through the graph and the count of elements"""
+
+    path: [str]
+    visited_any_lower_twice: bool = False
+
+    def add_node(self, node: str):
+        visited_twice = self.visited_any_lower_twice or (node.islower() and node in self.path)
+        return Path(path=[*self.path, node], visited_any_lower_twice=visited_twice)
 
 
 def read_graph(file):
@@ -22,17 +34,12 @@ def read_graph(file):
 
 
 def valid_edge_visit_small_twice(b, cur_path):
-    lower_counter = Counter(node for node in cur_path if node.islower())
-
-    if len(lower_counter) == sum(lower_counter.values()):
-        return True
-
-    return False
+    return not cur_path.visited_any_lower_twice
 
 
 def find_path(graph, cur_path, paths, edge_validator=None):
 
-    for a, b in graph.edges(cur_path[-1:]):
+    for a, b in graph.edges(cur_path.path[-1:]):
 
         # once you leave the start cave, you may not return to it
         if b == "start":
@@ -40,23 +47,24 @@ def find_path(graph, cur_path, paths, edge_validator=None):
 
         # once you reach the end cave, the path must end immediately
         if b == "end":
-            paths.append([*cur_path, b])
+            paths.append([*cur_path.path, b])
             continue
 
         # big caves can be visited any number of times
-        if b.isupper() or b not in cur_path or edge_validator and edge_validator(b, cur_path):
-            new_path = [*cur_path, b]
+        if b.isupper() or b not in cur_path.path or edge_validator and edge_validator(b, cur_path):
+            new_path = cur_path.add_node(b)
             find_path(graph, new_path, paths, edge_validator)
 
 
 def day12(file):
     graph = read_graph(file)
+
     small_once_paths = []
-    find_path(graph, ["start"], small_once_paths)
+    find_path(graph, Path(path=["start"]), small_once_paths)
     yield len(small_once_paths)
 
     small_twice_paths = []
-    find_path(graph, ["start"], small_twice_paths, valid_edge_visit_small_twice)
+    find_path(graph, Path(path=["start"]), small_twice_paths, valid_edge_visit_small_twice)
     yield len(small_twice_paths)
 
 
