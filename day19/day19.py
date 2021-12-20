07cd9c8808bfc48dd9a4ed2b261014257d0434b3
Wieland Morgenstern <wieland.morgenstern@hhi.fraunhofer.de>
2021-12-20 19:30:26 +0100

    day19: extract methods

--- day19/day19.py
+++ day19/day19.py
@@ -31,6 +31,9 @@ def read_scans(file):
     with open(file, "r") as f:
         for scan_chunk in f.read().split("\n\n"):
             beacons = np.loadtxt(StringIO(scan_chunk), skiprows=1, dtype=np.int16, delimiter=",")
+            
+            # use homogeneous coordinates
+            # to be able to find transforms with translations later
             ones = np.ones((beacons.shape[0], 1), dtype=np.int16)
             beacons_homogeneous = np.hstack((beacons, ones))
             scans.append(beacons_homogeneous)
@@ -39,6 +42,8 @@ def read_scans(file):
 
 def beacon_hash(beacon_list, reference_beacon):
     # hash function: manhatten distance to reference beacon
+    # all beacons lie on a grid, and this distance is transform invariant
+    # as all transforms are axis-aligned (only facing down/up/90 deg...)
     return np.abs(beacon_list - reference_beacon).sum(axis=1)
 
 def build_lookup(scan):
@@ -48,11 +53,7 @@ def build_lookup(scan):
         bh = beacon_hash(scan, ref_beacon)
         lookup_beacons.append(bh)
         lookup_sets.append(set(bh))
-    
     return lookup_beacons, lookup_sets
-
-def transform_beacons(beacons, orientation):
-    pass
 
 def find_transform(source_transform, source_scan, target_scan, source_lookups, target_lookups):
     matching_beacon_hashes = list(set(source_lookups) & set(target_lookups))
@@ -79,36 +80,7 @@ def find_transform(source_transform, source_scan, target_scan, source_lookups, t
 
     return transform
 
-
-
-def day19(file):
-
-    scans = read_scans(file)
-    scan_lookup_sets = []
-    scan_lookup_beacons = []
-    for scan in scans:
-        lb, ls = build_lookup(scan)
-        scan_lookup_beacons.append(lb)
-        scan_lookup_sets.append(ls)
-
-    scores = []
-
-    for query_scan_id, query_scan in enumerate(scans):
-        for qs_lookup_id, qs_lookup in enumerate(scan_lookup_sets[query_scan_id]):
-            for match_scan_id, match_lookups in enumerate(scan_lookup_sets):
-
-                if match_scan_id == query_scan_id:
-                    continue
-
-                for match_lookup_id, match_lookup in enumerate(match_lookups):
-                    score = len(qs_lookup & match_lookup)
-                    if score >= 12:
-                        scores.append((query_scan_id, qs_lookup_id, match_scan_id, match_lookup_id, score))
-
-    scores = sorted(scores, key=lambda x: x[3], reverse=True)
-    scdf = pd.DataFrame(scores, columns=["query_scan_id", "qs_lookup_id", "match_scan_id", "match_lookup_id", "score"])        
-    connections = scdf[scdf.score >= 12].groupby("query_scan_id").match_scan_id.unique()
-    
+def find_scan_transforms(scans, scan_lookup_beacons, scdf, connections):
     known_transforms = {0: np.eye(4, dtype=np.int16)}
     next_to_match = deque()
     for match in connections[0]:
@@ -122,37 +94,67 @@ def day19(file):
             known_transforms[target_id] = found_transform
             for match in connections[target_id]:
                 next_to_match.append((target_id, int(match)))
+    return known_transforms
+
+def find_scan_overlaps(scans, scan_lookup_sets):
+    scores = []
+
+    for query_scan_id, query_scan in enumerate(scans):
+        for qs_lookup_id, qs_lookup in enumerate(scan_lookup_sets[query_scan_id]):
+            for match_scan_id, match_lookups in enumerate(scan_lookup_sets):
+                if match_scan_id == query_scan_id:
+                    continue
+
+                for match_lookup_id, match_lookup in enumerate(match_lookups):
+                    score = len(qs_lookup & match_lookup)
+                    if score >= 12:
+                        scores.append((query_scan_id, qs_lookup_id, match_scan_id, match_lookup_id, score))
+
+    scores = sorted(scores, key=lambda x: x[3], reverse=True)
+    scdf = pd.DataFrame(scores, columns=["query_scan_id", "qs_lookup_id", "match_scan_id", "match_lookup_id", "score"])        
+    connections = scdf[scdf.score >= 12].groupby("query_scan_id").match_scan_id.unique()
+    return scdf,connections
+
+
+def day19_b(scan_transforms):
+    max_dist = 0
+    for a in scan_transforms.values():
+        for b in scan_transforms.values():
+            # last row of transformation matrix is translation
+            manhatten_dist = np.abs(a[-1] - b[-1]).sum()
+            max_dist = max(max_dist, manhatten_dist)
+    return max_dist
+
+def day19(file):
+
+    scans = read_scans(file)
+    scan_lookup_sets = []
+    scan_lookup_beacons = []
+    for scan in scans:
+        lb, ls = build_lookup(scan)
+        scan_lookup_beacons.append(lb)
+        scan_lookup_sets.append(ls)
+
+    scdf, connections = find_scan_overlaps(scans, scan_lookup_sets)
+    scan_transforms = find_scan_transforms(scans, scan_lookup_beacons, scdf, connections)
 
     all_beacons = []
-
     for scan_id, beacons in enumerate(scans):
-        transformed = beacons @ known_transforms[scan_id]
+        transformed = beacons @ scan_transforms[scan_id]
         all_beacons.append(transformed)
-
     all_beacons = np.concatenate(all_beacons)
     unique_rows = np.unique(all_beacons, axis=0)
-    # for all measurements
-    #  translate and rotate into scan-0 view
-    #  insert coords into set
-    # return len(coordset)
+
+    # day19 a: what is the number of beacons?
     yield unique_rows.shape[0]
 
     # day19 b: what is the largest Manhattan distance between any two scanners?
+    yield day19_b(scan_transforms)
 
-    max_dist = 0
-
-    for a in known_transforms.values():
-        for b in known_transforms.values():
-            manhatten_dist = np.abs(a[-1] - b[-1]).sum()
-            max_dist = max(max_dist, manhatten_dist)
-
-    yield max_dist
 
 @print_durations
 def run_expect(file, result_a, result_b):
-
     a, b = day19(file)
-
     print(f"Day 19a {file}: {a}")
     assert a == result_a
 
@@ -162,8 +164,6 @@ def run_expect(file, result_a, result_b):
 
 
 if __name__ == "__main__":
-
     run_expect("test_input.txt", 79, 3621)
     run_expect("input.txt", 306, 9764)
-
 
