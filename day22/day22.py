5a322d5179feeb3d0894566bc78356da519e4120
Wieland Morgenstern <wieland.morgenstern@hhi.fraunhofer.de>
2021-12-26 21:46:29 +0100

    day22: combine code for a and b, comments

--- day22/day22.py
+++ day22/day22.py
@@ -7,6 +7,22 @@ from tqdm.contrib import tzip
 
 # Puzzle: https://adventofcode.com/2021/day/22
 
+# reactor core is made up of a large 3-dimensional grid
+# made up entirely of cubes, one cube per integer 3-dimensional coordinate (x,y,z)
+# To reboot the reactor, you just need to set all of the cubes to either on or off by
+# following a list of reboot steps (your puzzle input), e.g.:
+#
+#     on x=10..12,y=10..12,z=10..12
+#     on x=11..13,y=11..13,z=11..13
+#     off x=9..11,y=9..11,z=9..11
+#     on x=10..10,y=10..10,z=10..10
+#
+# solution:
+# - identify unique xs, ys and zs
+# - map x->xs, y->ys, z->zs
+# - write the steps into a grid
+# - when counting the "on" cubes in the grid, multiply with their true size
+
 
 def read_reboot_steps(file):
 
@@ -21,35 +37,14 @@ def read_reboot_steps(file):
             steps_xyz = []
             for arg in coords_seg.split(","):
                 begin, end = arg[2:].split("..")
+                # note: endrange +1 to mark upper end of range (next coordinate)
                 steps_xyz.append((int(begin), int(end) + 1))
             coords.append(steps_xyz)
 
     return np.array(commands), np.array(coords)
 
 
-def day22a(commands, coords_list):
-
-    # The initialization procedure only uses cubes that have x, y, and z positions
-    # of at least -50 and at most 50. For now, ignore cubes outside this region.
-
-    reactor = np.zeros((101, 101, 101), dtype=bool)
-
-    # add 50 so we can use positive indeces
-    coords_list = coords_list + 50
-
-    for command, coords in zip(commands, coords_list):
-
-        # ignored
-        if coords.min() < 0 or coords.max() > 101:
-            continue
-
-        reactor[coords[0, 0] : coords[0, 1], coords[1, 0] : coords[1, 1], coords[2, 0] : coords[2, 1]] = command
-
-    #  how many cubes are on?
-    return reactor.sum()
-
-
-def day22b(commands, coords_list):
+def count_active_cubes(commands, coords_list):
 
     xs = coords_list[:, 0, :]
     ys = coords_list[:, 1, :]
@@ -83,15 +78,24 @@ def day22b(commands, coords_list):
 
 def day22(file):
 
-    # reactor core is made up of a large 3-dimensional grid
-    # made up entirely of cubes, one cube per integer 3-dimensional coordinate (x,y,z)
-    # To reboot the reactor, you just need to set all of the cubes to either on or off by
-    # following a list of reboot steps (your puzzle input)
-
     commands, coords_list = read_reboot_steps(file)
-    yield day22a(commands, coords_list)
 
-    yield day22b(commands, coords_list)
+    # day 22a: ignore cubes outside [-50, 50]
+    commands_50, coords_list_50 = list(
+        zip(
+            *[
+                (command, coords)
+                for (command, coords) in zip(commands, coords_list)
+                # up to coordinates 50 -> [:51]
+                if (coords.min() >= -50) and (coords.max() <= 51)
+            ]
+        )
+    )
+    # day 22a
+    yield count_active_cubes(commands_50, np.array(coords_list_50))
+
+    # day 22b: full grid
+    yield count_active_cubes(commands, coords_list)
 
 
 @print_durations
