ad188be5550e41fa4d279b59ac17b6947f561714
Wieland Morgenstern <wieland.morgenstern@hhi.fraunhofer.de>
2022-12-01 06:25:19 +0100

    2022-day01: original solution 🙈

--- 2022/day01/day01.py
+++ 2022/day01/day01.py
@@ -6,9 +6,37 @@ from collections import deque, Counter, defaultdict
 
 
 def compute(file):
-    yield None
+    with open(file, "r") as f:
+        lines = f.readlines()
 
-    yield None
+    lines = [line.strip() for line in lines]
+
+    cur_elf = 0
+    max_cal = 0
+    for line in lines:
+        if len(line):
+            cur_elf += int(line)
+        else:
+            max_cal = max(max_cal, cur_elf)
+            cur_elf = 0
+
+    yield max_cal
+
+    elves = []
+    cur_elf = 0
+    for line in lines:
+        if len(line):
+            cur_elf += int(line)
+        else:
+            elves.append(cur_elf)
+            cur_elf = 0
+
+    if cur_elf > 0:
+        elves.append(cur_elf)
+
+    elves = sorted(elves)
+
+    yield sum(elves[-3:])
 
 
 @print_durations
@@ -24,5 +52,5 @@ def run_expect(file, result_a, result_b):
 
 
 if __name__ == "__main__":
-    run_expect("test_input.txt", None, None)
-    run_expect("input.txt", None, None)
+    run_expect("test_input.txt", 24000, 45000)
+    run_expect("input.txt", 67016, 200116)
