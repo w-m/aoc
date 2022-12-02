58a5ee89dbe6180b8e122026e9525631697dcec3
Wieland Morgenstern <wieland.morgenstern@hhi.fraunhofer.de>
2022-12-02 08:23:42 +0100

    day02: 0, 1, 2 encoding of rock, paper, scissors

--- 2022/day02/day02.py
+++ 2022/day02/day02.py
@@ -6,82 +6,61 @@ import pandas as pd
 # https://adventofcode.com/2022/day/2
 
 
-def compute_a(file):
-    # Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock.
-    # If both players choose the same shape, the round instead ends in a draw.
-
-    # The first column is what your opponent is going to play:
-    # A for Rock, B for Paper, and C for Scissors
-
-    # The second column, you reason, must be what you should play in response:
-    # X for Rock, Y for Paper, and Z for Scissors.
-
-    df = pd.read_csv(file, header=None, names=["opponent", "you"], delim_whitespace=True)
-    df["win"] = df.apply(
-        lambda row: (row["opponent"] == "C" and row["you"] == "X")
-        or (row["opponent"] == "A" and row["you"] == "Y")
-        or (row["opponent"] == "B" and row["you"] == "Z"),
-        axis=1,
-    )
-
-    df["draw"] = df.apply(
-        lambda row: (row["opponent"] == "C" and row["you"] == "Z")
-        or (row["opponent"] == "A" and row["you"] == "X")
-        or (row["opponent"] == "B" and row["you"] == "Y"),
-        axis=1,
-    )
-
-    # Your total score is the sum of your scores for each round.
-
+def compute_score(df):
     # The score for a single round is the score for the shape you selected
     # (1 for Rock, 2 for Paper, and 3 for Scissors)
-
-    df["score"] = df.apply(lambda row: 1 if row["you"] == "X" else 2 if row["you"] == "Y" else 3, axis=1)
+    df["score"] = df["you"] + 1
 
     # plus the score for the outcome of the round
     # (0 if you lost, 3 if the round was a draw, and 6 if you won)
-    df["outcome"] = 0
-    df.loc[df["win"], "outcome"] = 6
-    df.loc[df["draw"], "outcome"] = 3
+    df["outcome"] = df["win"] * 6 + df["draw"] * 3
+
     df["total_score"] = df["score"] + df["outcome"]
 
+    # Your total score is the sum of your scores for each round.
     return df["total_score"].sum()
 
 
-def compute_b(file):
+def compute_a(file):
+    df = pd.read_csv(file, header=None, names=["opponent", "you"], delim_whitespace=True)
+
+    # The first column is what your opponent is going to play:
+    # A for Rock, B for Paper, and C for Scissors
+    df["opponent"] = df["opponent"].replace({"A": 0, "B": 1, "C": 2})
+
+    # The second column, you reason, must be what you should play in response:
+    # X for Rock, Y for Paper, and Z for Scissors.
+    df["you"] = df["you"].replace({"X": 0, "Y": 1, "Z": 2})
 
     # Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock.
+    df["win"] = df["you"] == (df["opponent"] + 1) % 3
+
     # If both players choose the same shape, the round instead ends in a draw.
+    df["draw"] = df["you"] == df["opponent"]
+
+    return compute_score(df)
+
+
+def compute_b(file):
+    df = pd.read_csv(file, header=None, names=["opponent", "end"], delim_whitespace=True)
+
+    # The first column is what your opponent is going to play:
+    # A for Rock, B for Paper, and C for Scissors
+    df["opponent"] = df["opponent"].replace({"A": 0, "B": 1, "C": 2})
 
     # "Anyway, the second column says how the round needs to end:
     # X means you need to lose, Y means you need to end the round in a draw,
     # and Z means you need to win. Good luck!"
+    df["lose"] = df["end"] == "X"
+    df["draw"] = df["end"] == "Y"
+    df["win"] = df["end"] == "Z"
 
-    df = pd.read_csv(file, header=None, names=["opponent", "end"], delim_whitespace=True)
-    df["draw"] = False
-    df.loc[df["end"] == "Y", "draw"] = True
-    df["win"] = False
-    df.loc[df["end"] == "Z", "win"] = True
-    df["lose"] = False
-    df.loc[df["end"] == "X", "lose"] = True
+    # default: lose
+    df["you"] = (df.opponent + 2) % 3
+    df.loc[df.draw, "you"] = df.loc[df.draw, "opponent"]
+    df.loc[df.win, "you"] = (df.loc[df.win, "opponent"] + 1) % 3
 
-    # The first column is what your opponent is going to play:
-    # A for Rock, B for Paper, and C for Scissors
-
-    df["you"] = ""
-    df.loc[df["draw"], "you"] = df["opponent"].replace({"A": "X", "B": "Y", "C": "Z"})
-    df.loc[df["lose"], "you"] = df["opponent"].replace({"A": "Z", "B": "X", "C": "Y"})
-    df.loc[df["win"], "you"] = df["opponent"].replace({"A": "Y", "B": "Z", "C": "X"})
-
-    df["score"] = df.apply(lambda row: 1 if row["you"] == "X" else 2 if row["you"] == "Y" else 3, axis=1)
-
-    # plus the score for the outcome of the round
-    # (0 if you lost, 3 if the round was a draw, and 6 if you won)
-    df["outcome"] = 0
-    df.loc[df["win"], "outcome"] = 6
-    df.loc[df["draw"], "outcome"] = 3
-    df["total_score"] = df["score"] + df["outcome"]
-    return df["total_score"].sum()
+    return compute_score(df)
 
 
 @print_durations
