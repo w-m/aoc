54ff18601e7c0631385966a8222a706a08728f34
Wieland Morgenstern <wieland.morgenstern@hhi.fraunhofer.de>
2022-12-05 06:57:50 +0100

    day04: condense and comment code

--- 2022/day04/day04.py
+++ 2022/day04/day04.py
@@ -6,60 +6,35 @@ import pandas as pd
 # https://adventofcode.com/2022/day/4
 
 
-def range_overlapping(x, y):
-    if x.start == x.stop or y.start == y.stop:
-        return False
-    return x.start < y.stop and y.start < x.stop
+def range_overlapping(range_a, range_b):
+    """Are the values in the two ranges overlapping at all?"""
+    return range_a.start < range_b.stop and range_b.start < range_a.stop
 
 
-def range_subset(range1, range2):
-    """Whether range1 is a subset of range2
-    https://stackoverflow.com/a/32481015/463796
-    """
-    if not range1:
-        return True  # empty range is subset of anything
-    if not range2:
-        return False  # non-empty range can't be subset of empty range
-    if len(range1) > 1 and range1.step % range2.step:
-        return False  # must have a single value or integer multiple step
-    return range1.start in range2 and range1[-1] in range2
+def range_subset(range_a, range_b):
+    """Whether range1 is a subset of range2"""
+    return range_a.start in range_b and range_a[-1] in range_b
 
 
-def str_to_range(rstr: str):
-    l, h = rstr.split("-")
+def str_to_range(range_str: str):
+    """range_str like "1-3" (elf range: inclusive) -> range(1, 4) Python"""
+    l, h = range_str.split("-")
     return range(int(l), int(h) + 1)
 
 
-def compute_a(file: str) -> int:
+def compute_query(file: str, query) -> int:
     with open(file) as f:
-        lines = f.read().splitlines()
+        ranges = (map(str_to_range, line.strip().split(",")) for line in f)
+        return sum(query(range_a, range_b) for range_a, range_b in ranges)
 
-    sum = 0
-    for line in lines:
-        a, b = line.split(",")
 
-        ar = str_to_range(a)
-        br = str_to_range(b)
-        sum += range_subset(br, ar) or range_subset(ar, br)
-
-    return sum
+def compute_a(file: str) -> int:
+    is_either_subset = lambda ar, br: range_subset(ar, br) or range_subset(br, ar)
+    return compute_query(file, is_either_subset)
 
 
 def compute_b(file):
-    with open(file) as f:
-        lines = f.read().splitlines()
-
-    sum = 0
-    for line in lines:
-        a, b = line.split(",")
-
-        ar = str_to_range(a)
-        br = str_to_range(b)
-        overlap = range_overlapping(ar, br)
-        print(f"{ar}, {br}, {overlap}")
-        sum += overlap
-
-    return sum
+    return compute_query(file, range_overlapping)
 
 
 @print_durations
@@ -82,4 +57,4 @@ def run_expect(file, result_a, result_b):
 
 if __name__ == "__main__":
     run_expect("test_input.txt", 2, 4)
-    run_expect("input.txt", 556, -1)
+    run_expect("input.txt", 556, 876)
