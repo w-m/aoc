5436dceac209d8e895a42970e915e0ccf689ad2d
Wieland Morgenstern <wieland.morgenstern@hhi.fraunhofer.de>
2022-12-06 06:23:49 +0100

    day06: add the rest of the examples

--- 2022/day06/day06.py
+++ 2022/day06/day06.py
@@ -6,7 +6,12 @@ from collections import deque
 # https://adventofcode.com/2022/day/6
 
 
-def find_start_marker(line: str, unique_consecutive_chars: int) -> int:
+def read_single_line(file) -> str:
+    with open(file) as f:
+        return f.read().strip()
+
+
+def find_start_marker(datastream: str, unique_consecutive_chars: int) -> int:
 
     # The device will send your subroutine a datastream buffer (your puzzle input)
 
@@ -17,7 +22,7 @@ def find_start_marker(line: str, unique_consecutive_chars: int) -> int:
     # of the buffer to the end of the first such four-character marker.
 
     deq = deque([], maxlen=unique_consecutive_chars)
-    for i, char in enumerate(line):
+    for i, char in enumerate(datastream):
         deq.append(char)
         if len(set(deq)) == unique_consecutive_chars:
             return i + 1
@@ -25,30 +30,30 @@ def find_start_marker(line: str, unique_consecutive_chars: int) -> int:
 
 
 @print_durations
-def compute(file) -> Iterator[Optional[int]]:
-
-    with open(file) as f:
-        line = f.read().strip()
+def compute(datastream) -> Iterator[Optional[int]]:
 
     # start of packet marker
-    yield find_start_marker(line, 4)
+    yield find_start_marker(datastream, 4)
 
     # start of message marker
-    yield find_start_marker(line, 14)
+    yield find_start_marker(datastream, 14)
 
 
 @print_durations
-def run_expect(file, result_a, result_b):
-    a, b = compute(file)
-    prefix = os.path.basename(__file__)
-    print(f"{prefix} {file}: {a}")
+def run_expect(datastream, result_a, result_b):
+    a, b = compute(datastream)
+    print(f"{datastream[:21]}...: {a}")
     assert a == result_a
 
     if result_b:
-        print(f"{prefix} {file}: {b}")
+        print(f"{datastream[:21]}...: {b}")
         assert b == result_b
 
 
 if __name__ == "__main__":
-    run_expect("test_input.txt", 7, 19)
-    run_expect("input.txt", 1275, 3605)
+    run_expect("bvwbjplbgvbhsrlpgdmjqwftvncz", 5, 23)
+    run_expect("nppdvjthqldpwncqszvftbrmjlhg", 6, 23)
+    run_expect("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10, 29)
+    run_expect("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11, 26)
+    run_expect(read_single_line("test_input.txt"), 7, 19)
+    run_expect(read_single_line("input.txt"), 1275, 3605)
